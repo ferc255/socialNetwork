f8390011753e691e43644c5ec6bf92105584626a
ferc_prod <django@tuna.com.ru>
2017-06-23 10:09:23 +0000

    Small fixes on info-page

--- devSettings.py
+++ devSettings.py
@@ -78,12 +78,27 @@ WSGI_APPLICATION = 'socn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+"""
+DATABASES = {
+      'default': {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': 'socndev',
+                'USER': 'alex',
+                'PASSWORD': '1',
+                'HOST': 'localhost',
+                'PORT': '',
+                      }
+    }
+"""
 
 
 # Password validation
@@ -133,7 +148,14 @@ ANON_URLS = \
 (
     r'^account/login/$',
     r'^account/register/$',
+    r'^$',
 )
+
+LOGIN_EXEMPT_URLS = \
+[
+    r'^account/login/$',
+    r'^account/register/$',
+]
 
 STATICFILES_DIRS = \
 [
