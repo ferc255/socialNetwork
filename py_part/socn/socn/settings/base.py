534eb802445f4c788d72d32519667e54c08a7e83
ferc255 <alekssslutskiy@yandex.ru>
2017-11-03 03:53:18 -0400

    Settings were structured

--- py_part/socn/socn/settings/base.py
+++ py_part/socn/socn/settings/base.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/aleksey/socialnetwork/py_part/socn/static/'
+# STATIC_ROOT = '/home/aleksey/socialnetwork/py_part/socn/static/'
 
 MEDIA_ROOT = '/home/aleksey/socialnetwork/py_part/socn/media/'
 
--- py_part/socn/socn/settings/dev.py
+++ py_part/socn/socn/settings/dev.py
@@ -10,3 +10,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+try:
+    from socn.settings.local import *
+except:
+    pass
